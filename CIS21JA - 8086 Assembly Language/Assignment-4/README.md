0bcefc8ee5c357e04a29095a405cdb7ad8b66944
Rashmi Baheti <baheti.rash123@gmail.com>
2017-08-06 17:49:37 -0700

    add 3rd readme file

---
+++ CIS21JA - 8086 Assembly Language/Assignment-4/README.md
@@ -0,0 +1,12 @@
+Assignment 4
+
+Question:  
+Write a complete program that:  
+1. Promts the user to enter 10 numbers.  
+2. saves those numbers in a 32 bit integer array.  
+3. Calculates the sum of the numbers and displays it.  
+4. Calcualtes the mean of the array and displays it.  
+5. Prints the array with the same order it was enterd.  
+6. Rotates the members in the array forward one position for  9 times. so the last rotation will display the array in reversed order. Print the array after each rotation.  
+
+Don't use any shift or rotate instuctions which we have not coverd yet. You need to use loop and indexed addressing. All you work should be on the original array. Don't make a copy of the array at any time. Add comments to make your program easy to read.
--- CIS21JA - 8086 Assembly Language/Assignment-4/main.asm
+++ CIS21JA - 8086 Assembly Language/Assignment-4/main.asm
@@ -1,30 +1,3 @@
-; Program template
-TITLE   Assignment 4
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; name: Rashmi Baheti
-; date: 04/29/2016
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-Comment !
-Write a complete program that:
-	1. Promts the user to enter 10 numbers.
-	2. saves those numbers in a 32 bit integer array.
-	3. Calculates the sum of the numbers and displays it.
-	4. Calcualtes the mean of the array and displays it.
-	5. Prints the array with the same order it was enterd.
-	6. Rotates the members in the array forward one position for 
-	   9 times. so the last rotation will display the array in 
-	   reversed order. Print the array after each rotation.
-	   check the sample run.
-	
-	  
-	  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	  Don't use any shift or rotate instuctions which we have
-	  not coverd yet. You need to use loop and indexed addressing.
-	  All you work should be on the original array. Don't make 
-	  a copy of the array at any time.
-	  Add comments to make your program easy to read.
-!
-
 INCLUDE Irvine32.inc
 
 .DATA
